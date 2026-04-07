19a77df0e2730e640f74586a2b1ac216cae28669
Abdisa Awel <abdisaawel313@gmail.com>
2026-04-07 16:35:57 +0300

    Initialize Exprress backend and database connection

--- backend/src/middleware/errorMiddleware.ts
+++ backend/src/middleware/errorMiddleware.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from 'express';
+import { type Request, type Response, type NextFunction } from 'express';
 
 const notFound = (req: Request, res: Response, next: NextFunction) => {
     const error = new Error(`Not Found - ${req.originalUrl}`);
